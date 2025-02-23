42968240ed6e7fd4339a5bd29546c48296b6e2fb
xlr8tanmay <tchamrolia@gmail.com>
2025-02-24 00:48:59 +0530

    BinaryTrees initial setup

---
+++ BinaryTrees/TreeNode.java
@@ -0,0 +1,12 @@
+package BinaryTrees;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
